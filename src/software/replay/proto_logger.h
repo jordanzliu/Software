810546b4a1847c921b929999e5c72e698fe14ec2
Jordan Liu <jordan.zliu@gmail.com>
2020-04-05 21:17:18 -0700

    fix some stuff

---
+++ src/software/replay/proto_logger.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <fstream>
+#include <google/protobuf/message.h>
+#include "software/logger/init.h"
+#include "software/multithreading/threaded_observer.h"
+
+template <class ProtobufMessage>
+class ProtoLogger : public ThreadedObserver<ProtobufMessage> {
+    static_assert(std::is_base_of_v<google::protobuf::Message, ProtobufMessage>,
+            "ProtoLogger only works for protobuf messages!");
+public:
+    ProtoLogger() = delete;
+
+    explicit ProtoLogger(const std::string &filename): output_file_stream(filename) {
+        LOG(INFO) << "Logging " << typeid(ProtobufMessage).name() << " to " << filename;
+    }
+
+private:
+    void onValueReceived(ProtobufMessage val) override;
+
+    std::ofstream output_file_stream;
+};
+
+template<class ProtobufMessage>
+void ProtoLogger<ProtobufMessage>::onValueReceived(ProtobufMessage val) {
+    if (val.SerializeToOstream(&output_file_stream))
+    {
+        throw std::string("Failed to log protobuf to file");
+    } else {
+        LOG(INFO) << "Logged a protobuf message";
+    }
+}
